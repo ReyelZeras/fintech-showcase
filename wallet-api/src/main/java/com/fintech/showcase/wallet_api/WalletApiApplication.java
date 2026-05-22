3bd24c7272ca444b7ed289610da340950ecbfc23
ReyelZeras <rey2504@outlook.com>
2026-05-22 09:18:07 -0300

    feat: implementa cache distribuido com Redis e estrategia de eviccao de dados obsoletos

--- wallet-api/src/main/java/com/fintech/showcase/wallet_api/WalletApiApplication.java
+++ wallet-api/src/main/java/com/fintech/showcase/wallet_api/WalletApiApplication.java
@@ -2,8 +2,10 @@ package com.fintech.showcase.wallet_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // Habilita o suporte a Cache do Spring
 public class WalletApiApplication {
 
 	public static void main(String[] args) {
