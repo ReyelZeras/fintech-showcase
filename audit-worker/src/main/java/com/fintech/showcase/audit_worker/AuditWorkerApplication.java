8df1419eba4855bca600877490703afa67d3ba1a
ReyelZeras <rey2504@outlook.com>
2026-05-21 21:52:34 -0300

    chore: setup inicial do projeto com estrutura de monorepo e infra docker

---
+++ audit-worker/src/main/java/com/fintech/showcase/audit_worker/AuditWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.showcase.audit_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditWorkerApplication.class, args);
+	}
+
+}
---
+++ audit-worker/src/test/java/com/fintech/showcase/audit_worker/AuditWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.showcase.audit_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
